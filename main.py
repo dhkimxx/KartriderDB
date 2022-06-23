64906e005bb728b7b450bca0293b05ade774486b
dhkimxx <79616878+dhkimxx@users.noreply.github.com>
2022-06-23 13:31:35 +0000

    2022-06-23

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from getMatchId import getAllMatchId
 from getMatchInfo import getMatchInfo
 from getNameId import nicknameToId, idToNickname
 
-start = '2021-08-23 20:00:00'
-end = '2021-08-23 21:00:00'
+start = '2022-02-22 19:00:00'
+end = '2022-02-22 20:00:00'
 match_id_list = getAllMatchId(start, end)
 for match_id in match_id_list:
   getMatchInfo(match_id)
